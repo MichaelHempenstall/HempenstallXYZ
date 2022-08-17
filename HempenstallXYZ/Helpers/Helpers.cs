0a0bcea44e95b165d03b6311a0d6c363ba26b159
MichaelHempenstall <mhempenstall@gmail.com>
2022-08-17 22:03:23 +0100

    Added some embedded data from https://data.gov.ie/dataset

---
+++ HempenstallXYZ/Helpers/Helpers.cs
@@ -0,0 +1,31 @@
+﻿namespace HempenstallXYZ.Helpers
+{
+    public class Helpers
+    {
+        private string? _gitHash = null;
+        //Gets the GitHash from the build
+        public string GitHash
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_gitHash))
+                {
+                    var version = "1.0.0+LOCALBUILD"; // Dummy version for local dev
+                    //var appAssembly = typeof(AppVersionInfo).Assembly;
+                    //var infoVerAttr = (AssemblyInformationalVersionAttribute)appAssembly
+                    //.GetCustomAttributes(typeof(AssemblyInformationalVersionAttribute)).FirstOrDefault();
+
+                    //if (infoVerAttr != null && infoVerAttr.InformationalVersion.Length > 6)
+                    //{
+                    //    // Hash is embedded in the version after a '+' symbol, e.g. 1.0.0+a34a913742f8845d3da5309b7b17242222d41a21
+                    //    version = infoVerAttr.InformationalVersion;
+                    //}
+                    _gitHash = version.Substring(version.IndexOf('+') + 1);
+
+                }
+
+                return _gitHash;
+            }
+        }
+    }
+}
---
+++ HempenstallXYZ/Pages/SampleHouseholdSavings.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HempenstallXYZ.Pages
+{
+    public class SampleHouseholdSavingsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
